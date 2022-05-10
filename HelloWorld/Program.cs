b028e0f602d1c261fe985c43da49b70717e68d16
Igor dos Santos Benevides <105222997+IgorBenevides@users.noreply.github.com>
2022-05-10 13:57:51 -0300

    Initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            // int - -156 como 333545
+            // Float -
+
+
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Zelhao babaca");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# HelloWorld
+ estudos C#
